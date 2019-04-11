cfead63cb29fc18910a68f6cbca29c81a0ef102a
Vasyl Sovyak <vasyl.sovyak@gameloft.com>
2019-04-11 15:32:13 +0300

    Initial commit

---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SoulShockers\ChuckNorrisJokes\Tests;
+
+use PHPUnit\Framework\TestCase;
+use SoulShockers\ChuckNorrisJokes\JokeFactory;
+
+class JokeFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory([
+            'This is a joke'
+        ]);
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+    }
+
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+            'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+            'Chuck Norris counted to infinity... Twice.',
+            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
+        ];
+
+        $jokes = new JokeFactory();
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
+}
