ba42bcd3c62202f4f1590e6b56bb3550fb2e1a00
Vasyl Sovyak <soulshockers@gmail.com>
2019-05-07 18:29:31 +0300

    Add auto-discovery for laravel

---
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace SoulShockers\ChuckNorrisJokes;
+
+use Illuminate\Support\ServiceProvider;
+
+class ChuckNorrisJokesServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind('chuck-norris', function () {
+            return new JokeFactory();
+        });
+    }
+}
---
+++ src/Facades/ChuckNorris.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace SoulShockers\ChuckNorrisJokes\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class ChuckNorris extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'chuck-norris';
+    }
+}
